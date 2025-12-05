1ea31bc197d79afba131a6dc2245e43a3404b2c0
Puripan <chopun2547@gmail.com>
2025-12-05 14:51:38 +0200

    feat: Initial passing unit test and Jest setup

---
+++ test/first.test.js
@@ -0,0 +1,9 @@
+// You might need an import statement here later, but for a guaranteed pass, start with this:
+
+describe('Initial Setup Test', () => {
+    // This is the actual test case Jest is looking for
+    test('1 plus 1 equals 2 (placeholder test)', () => {
+        // The 'expect' function performs the assertion
+        expect(1 + 1).toBe(2);
+    });
+});
---
+++ utils.js
@@ -0,0 +1,18 @@
+// A simple mock test structure to start with
+// You must adjust the path ('../src/...') based on where your library files are.
+
+// Example: Import a function named 'add' from the library
+// const { add } = require('../src/utils'); 
+
+// Use Jest's 'test' function
+test('A simple test that should always pass', () => {
+    expect(1 + 1).toBe(2);
+});
+
+/* // Example of a real test for the library:
+const { someFunction } = require('../path/to/rnnvm/file');
+
+test('someFunction should return 5 when given 2 and 3', () => {
+    expect(someFunction(2, 3)).toBe(5);
+});
+*/
